398dfe113ed2dd2d935bd5322ccfcebd53928ca0
blando6 <blandinevigan9@gmail.com>
2023-05-21 16:43:26 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,12 @@
 /**
  * main - A program that prints a line with puts function
  
- * Return: 0 (Success)
+ * Return:00uccess)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle
 ");
-return (0);
+(return(0)Success);
 }
+
