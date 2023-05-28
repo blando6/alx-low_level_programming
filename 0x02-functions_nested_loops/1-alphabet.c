bb638945382a2816fb376f655eb0ec73a44e10cc
blando6 <blandinevigan9@gmail.com>
2023-05-28 02:04:04 -0700

    ds

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (succes)
+ */
+void print_alphabet(void)
+{
+	int n;
+	char c;
+	for(c = 'a'; c <= 'z';c++)
+ 		{
+		_putchar(n);
+		}
+		_putchar('\n');
+}
